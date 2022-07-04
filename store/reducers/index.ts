3ee42bedeaf4381b6e7939787096093ea0d0ad7c
michaelwang110 <michael.mhw1@gmail.com>
2022-07-04 23:31:51 +1000

    set up global state

--- store/reducers/index.ts
+++ store/reducers/index.ts
@@ -1,18 +1,19 @@
-const GlobalState = {
-    data: ""
-};
-
-const gameReducer = (state = GlobalState, action) => {
-    switch (action.type) {
-        case "MOVE_RIGHT":
-            /**
-             * Perform a certain set of operations
-             */
-            return {
-                ...state, data: action.payload
-            };
-
-        default:
-            return state;
-    }
+interface ISnakeCoord {
+  x: number;
+  y: number;
 }
+
+export interface IGlobalState {
+  snake: ISnakeCoord[] | [];
+}
+
+const globalState: IGlobalState = {
+  //Postion of the entire snake
+  snake: [
+    { x: 580, y: 300 },
+    { x: 560, y: 300 },
+    { x: 540, y: 300 },
+    { x: 520, y: 300 },
+    { x: 500, y: 300 },
+  ],
+};
